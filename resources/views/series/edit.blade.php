5ba41871dcb1e3f0b3d00d6909010be748ccb386
Conan Torres <conantorreswf@gmail.com>
2024-03-04 20:48:52 -0300

    deleta form.blade.php

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,5 +1,16 @@
-<x-layout title='Editar Série'>
-    <a href='{{ route('series.index') }}' class='btn btn-dark mb-2'>Lista de séries</a>
-    <x-series.form :action="route('series.update', $serie->id)" :nome='$serie->nome' :update="true"/>
+<x-layout title="Editar Série '{!! $serie->nome !!}'">
+    <a href='{{ route('series.index') }}' class='btn btn-dark mb-2'>Lista de séries</a>    
     
+    <form action="{{ route('series.update', $serie->id) }}" method='post' class=''>
+
+        @csrf
+        @method('PUT')
+
+        <div class='mb-3'>
+            <label for='nome' class='form-label'>Nome: </label>
+            <input type='text' id='nome' name='nome' class='form-control' @isset($serie->nome) value='{{ $serie->nome }}' @endisset>
+        </div>
+        <button type='submit' class='btn btn-primary'>Enviar</button>
+    </form> 
+
 </x-layout>
