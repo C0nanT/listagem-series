9cf6fc0ef549a6d1b7e6b1aa0de3e19d1d1c2749
Conan Torres <conantorreswf@gmail.com>
2024-03-28 20:35:56 -0300

    bulk insert

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -27,17 +29,26 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request){
 
         $serie = Series::create($request->all());
+        $seasons = [];
         for ($i = 1; $i <= $request->seasons; $i++){
-            $season = $serie->seasons()->create([
+            $seasons [] = [
+                'series_id' => $serie->id,
                 'number' => $i,
-            ]);
+            ];
+        }
+        Season::insert($seasons);
 
+        $episodes = [];
+        foreach ($serie->seasons as $season){
             for ($j = 1; $j <= $request->episodes; $j++){
-                $season->episodes()->create([
-                'number' => $j
-            ]);
+                $episodes [] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
             }
         }
+        Episode::insert($episodes);
+
         $request->session();
 
         return to_route('series.index')
